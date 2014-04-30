1d7080615f0d6c338ef3562f5a5c1e8258b907b3
DERRIER Dominique <d.derrier@linkbynet.com>
2014-04-30 19:58:54 -0400

    Parse Csv file

--- CalGen.py
+++ CalGen.py
@@ -25,20 +25,37 @@ evt=Event()
 cal['X-CALENDARSERVER-ACCESS']		="CONFIDENTIAL"
 # CONFIDENTIAL | PUBLIC
 
-evt.add('dtstart',datetime(2014,04,29,20,0,0,tzinfo=pytz.timezone('America/Montreal')))
-evt.add('dtend',datetime(2014,04,29,21,0,0,tzinfo=pytz.timezone('America/Montreal')))
-evt.add('summary','Evenement secret')
-evt.add('description','Generated Evenement')
+#evt.add('dtstart',datetime(2014,04,29,20,0,0,tzinfo=pytz.timezone('America/Montreal')))
+#evt.add('dtend',datetime(2014,04,29,21,0,0,tzinfo=pytz.timezone('America/Montreal')))
+#evt.add('summary','Evenement secret')
+#evt.add('description','Generated Evenement')
+#
+#cal.add_component(evt)
 
-cal.add_component(evt)
+
+#evt2=Event()
+#evt2.add('summary','Multiple Day')
+#evt2.add('dtstart;VALUE=DATE','20140429')
+#evt2.add('dtend;VALUE=DATE','20140501')
+#cal.add_component(evt2)
 
 
-evt2=Event()
-evt2.add('summary','Multiple Day')
-evt2.add('dtstart;VALUE=DATE','20140429')
-evt2.add('dtend;VALUE=DATE','20140501')
-cal.add_component(evt2)
+from datetime import timedelta
 
+f=open('dd.csv','r')
+# read CVS file > DATE ; Event to produce calendar | full day
+for a in f.read().splitlines():
+	evt=Event()
+	(t,v)= a.split(';')
+	(d,m,y)=t.split('/')
+	date = datetime(int(y),int(m),int(d))
+	dstart= date.strftime('%Y%m%d')
+	dtend= (date+timedelta(1)).strftime('%Y%m%d')
+	evt.add('summary',v)
+	evt.add('dtstart;VALUE=DATE',dstart)
+	evt.add('dtend;VALUE=DATE',dtend)
+	cal.add_component(evt)
+f.close()
 
 f=open('example.ics','wb')
 f.write(cal.to_ical())
